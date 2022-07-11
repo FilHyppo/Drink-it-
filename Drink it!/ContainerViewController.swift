c48168f3b782930aeefa7ff2a1a88a7832cd4683
filippobologna <filippo@fam.bologna.it>
2022-07-12 00:08:00 +0200

    Initial Commit

---
+++ Drink it!/ContainerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drink it!
+//
+//  Created by Filippo Bologna on 11/07/22.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
---
+++ Drink it!UITests/Drink_it_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Drink_it_UITestsLaunchTests.swift
+//  Drink it!UITests
+//
+//  Created by Filippo Bologna on 11/07/22.
+//
+
+import XCTest
+
+class Drink_it_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
