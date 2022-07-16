a7e37ac338c7a89c8c2400b0ac0f2df59bc6e32b
filippobologna <filippo@fam.bologna.it>
2022-07-16 15:18:57 +0200

    a

--- Drink it!/ContainerViewController.swift
+++ Drink it!/ContainerViewController.swift
@@ -11,7 +11,7 @@ class ContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .darkGray
     }
 
 
